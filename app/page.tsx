1942e73c1108fe4d4f81f91e88724605cf95b683
asavage <alex.savage@shinetech.com>
2024-04-29 15:56:34 +1000

    copy + analytics

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             </p>
             <hr className="my-6 border-gray-300" />
             <p className="text-md text-gray-600">
-            Understanding your organization&apos;s current AI capabilities is crucial in today&apos;s tech-driven market.<br></br><br></br>Drawing upon established frameworks by industry leaders like <em>IBM</em> and <em>Accenture</em>, we offer sophisticated assessment through a tailored questionnaire. This tool is designed to pinpoint your operational strengths and areas for growth in AI.
+            Understanding your organization&apos;s current AI capabilities is crucial in today&apos;s tech-driven market.<br></br><br></br>Drawing upon established frameworks by industry leaders like <em>IBM</em> and <em>Accenture</em>, we offer assessment through a tailored questionnaire. This tool is designed to pinpoint your operational strengths and areas for growth in AI. 
             </p>
           </div>
         </div>
@@ -24,7 +24,7 @@ export default function Home() {
         <div className="mx-auto max-w-4xl w-full p-2 bg-white rounded-lg border border-gray-300 shadow-lg overflow-auto transition-transform duration-300 ease-in-out">
           <OpenAIAssistant
             assistantId="asst_F0BXMk3wkyyTWBYzjonmQpTe"
-            greeting={`🚀 Hello! I'm *Aimme Pointsout*, a master AI architect, strategist, and engineer.\n\nToday, I'm here to assist with a detailed assessment of your business' *AI Maturity Model* (AIMM). Are you ready to discover how AI can elevate your operations?\n\nType **'Begin'** to start the 15min assessment! 🚀
+            greeting={`🚀 Hello! I'm *Aimme Pointsout*, a master AI architect, strategist, and engineer.\n\nToday, I would like to ask you a few questions and in return I will provide you with a your business' AI Maturity Model (AIMM). Are you ready to discover how AI can elevate your operations?\n\nType **'Begin'** to start the 5min assessment! Feel free to answer the questions conversationally… 🚀
             `}
             messageLimit={50}
           />
