36a744da699fa55bbe03c4727a18a79f2ecaedf4
asavage <alex.savage@shinetech.com>
2024-06-04 15:35:47 +1000

    delete treemap code

--- app/ui/high-level-analytics/DropoffAnalytics.tsx
+++ app/ui/high-level-analytics/DropoffAnalytics.tsx
@@ -29,18 +29,7 @@ const DropoffAnalytics: React.FC<DropoffProps> = ({ dropoff_points }) => {
 
   return (
     <div className="w-full">
-      {treeData.map((segment, index) => (
-        <div key={index} className="border border-gray-200 rounded-lg p-4">
-          <div className="font-bold text-lg">{segment.name}</div>
-          <div className="flex flex-wrap">
-            {segment.children && segment.children.map((step, stepIndex) => (
-              <div key={stepIndex} className={`p-2 m-1 bg-blue-500 text-white rounded-lg text-sm flex-1`} style={{ flexBasis: `${step.size}%` }}>
-                {step.name} - {step.size}
-              </div>
-            ))}
-          </div>
-        </div>
-      ))}
+      {/* TODO impliment recharts treemap */}
     </div>
   );
 };
